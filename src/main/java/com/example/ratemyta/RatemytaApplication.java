03b46ce515e94c43b0fcfb330ce01b990a3dfe26
SamCarrillo9 <sammysambo9@icloud.com>
2025-11-08 20:08:53 -0500

    Initial commit

---
+++ src/main/java/com/example/ratemyta/RatemytaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ratemyta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatemytaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatemytaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ratemyta/RatemytaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ratemyta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatemytaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
